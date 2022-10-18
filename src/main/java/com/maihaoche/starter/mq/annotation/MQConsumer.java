8a13529cfcf2bcbf1c7474300004bf8eefaa9e77
elon <569199386@qq.com>
2022-10-18 15:19:54 +0800

    feat(conf): 去掉MQConsumer 和MQProducer注解上 的  @component, 用来支持按需装配

--- src/main/java/com/maihaoche/starter/mq/annotation/MQConsumer.java
+++ src/main/java/com/maihaoche/starter/mq/annotation/MQConsumer.java
@@ -1,6 +1,7 @@
 package com.maihaoche.starter.mq.annotation;
 
 import com.maihaoche.starter.mq.base.MessageExtConst;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.stereotype.Component;
 
 import java.lang.annotation.*;
@@ -8,11 +9,13 @@ import java.lang.annotation.*;
 /**
  * Created by yipin on 2017/6/27.
  * RocketMQ消费者自动装配注解
+ *
+ * 关闭 Component 注解，用来可以通过 ConditionalOn * 的注解进行按需配置  by guzhandong at 2022年10月18日15:14:46
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Component
+//@Component
 public @interface MQConsumer {
     String consumerGroup();
     String topic();
--- src/main/java/com/maihaoche/starter/mq/annotation/MQProducer.java
+++ src/main/java/com/maihaoche/starter/mq/annotation/MQProducer.java
@@ -7,10 +7,12 @@ import java.lang.annotation.*;
 /**
  * Created by yipin on 2017/6/27.
  * RocketMQ生产者自动装配注解
+ *
+ * 关闭 Component 注解，用来可以通过 ConditionalOn * 的注解进行按需配置  by guzhandong at 2022年10月18日15:14:46
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Component
+//@Component
 public @interface MQProducer {
 }
